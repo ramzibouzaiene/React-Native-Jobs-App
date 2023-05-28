8e29f253717ab71cd632e0d40129e98dd4f82fa6
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-27 23:43:46 -0700

    :tada: setup popular job components UI and logic

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -10,11 +10,11 @@ import { COLORS, SIZES } from "../../../constants";
 import styles from "./popularjobs.style";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 import { useRouter } from "expo-router";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch();
   return (
     <View style={styles.container}>
       <View style={styles.header}>
---
+++ hook/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/search/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "7bec76f9cdmsh92c628acef8316cp1dad8ejsnf04e23e935c8",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
