d8546f47b2f777851fcff5ad87441bb20f9bbe4c
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-28 12:15:49 -0700

    :tada: setup near by job ui and logic

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -12,8 +12,8 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
           source={{
-            uri: checkImageURL(job.employer_logo)
-              ? job.employer_logo
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           resizeMode="contain"
